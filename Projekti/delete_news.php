bba99c840fabb10bb4e4b92875a3c24eabb85bf9
Rilind-Pirana <rilindpirana@gmail.com>
2026-02-02 16:12:46 +0100

    Add files via upload

--- Projekti/delete_news.php
+++ Projekti/delete_news.php
@@ -4,14 +4,11 @@ require_once "News1.php";
 
 $newsObj = new News($conn);
 
-// Merr ID nga URL
 $id = $_GET['id'] ?? 0;
 
-// Kontroll i thjeshtë
 if ($id > 0) {
     $newsObj->delete($id);
 }
 
-// Redirect pas fshirjes
 header("Location: read_news.php");
 exit;
